a1aed59868565153195b609439b7bd01444a94fb
Thanhdev87 <nguyenthanhdev87@gmail.com>
2023-07-14 19:23:06 +0700

    fith commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//I. Sử dụng Github lưu code********************************
 //1. Đăng ký tài khoản git hub
 //2. Tải phần mềm git hub
 //3. Cấu hình Git
@@ -14,6 +15,18 @@
  * git commit -m "nội dung lần commit" //Chạy mỗi lần commit đẩy dữ liệu lên github
  * git remote add origin https://đường dẫn đến repository trên git hub //kết nối đến github (chạy lần đầu)
  * git push origin master //Chạy mỗi lần đẩy dữ liệu lên github
+ */
+//II. Sử dụng công cụ code Visual studio code và cài môi trường Nodejs
+/**
+ * 1. Cấu hình ban đầu cho Visual studio code
+ * + Vào File -> Preference -> Settings: Tìm từ khóa Format -> Tích chọn Format on save
+ * + Cài các phần mở rộng: Auto Rename Tag; Bootstrap 4, Fontawesome 4...; formate: CSS/LESS/SCSS...; Live Server; Material Icon Theme
+ */
+//III. Cài react
+/**
+ * 1. Mở folder để chứa opject -> Chạy terminal trong visual studio code/ hoặc chạy cmd
+ * 2. npx create-react-app "ten-app-muon-dat"
+ * 3. npm start //chạy app lần đầu
  */
 
 console.log("Hello GitHub");
