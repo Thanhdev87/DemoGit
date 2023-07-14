42591d3c8183badc117acb70599d65afc0583bec
Thanhdev87 <nguyenthanhdev87@gmail.com>
2023-07-14 18:14:07 +0700

    four commit

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-// Khởi tạo kết nối
+//1. Đăng ký tài khoản git hub
+//2. Tải phần mềm git hub
+//3. Cấu hình Git
+/**
+ * Chuột phải vào folder bất kỳ -> Git bash here //mở cửa sổ gõ lệnh
+ * git config --global user.name "Tên user tài khoản github"
+ * git config --global user.email "Tên email github"
+ * git config --list //xem lại các cấu hình vừa cài đặt
+ */
+//4. Khởi tạo kết nối
 /**
  * git init //Khởi tạo folder git (chạy lần đầu)
- * git add .
+ * git add . //Chạy mỗi lần đẩy dữ liệu lên github
  * git commit -m "nội dung lần commit" //Chạy mỗi lần commit đẩy dữ liệu lên github
  * git remote add origin https://đường dẫn đến repository trên git hub //kết nối đến github (chạy lần đầu)
  * git push origin master //Chạy mỗi lần đẩy dữ liệu lên github
