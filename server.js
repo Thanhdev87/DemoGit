f7d0834ef751d6507c75edc4c88a28999319c358
Thanhdev87 <nguyenthanhdev87@gmail.com>
2023-07-14 17:30:34 +0700

    third commit

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+// Khởi tạo kết nối
+/**
+ * git init //Khởi tạo folder git (chạy lần đầu)
+ * git add .
+ * git commit -m "nội dung lần commit" //Chạy mỗi lần commit đẩy dữ liệu lên github
+ * git remote add origin https://đường dẫn đến repository trên git hub //kết nối đến github (chạy lần đầu)
+ * git push origin master //Chạy mỗi lần đẩy dữ liệu lên github
+ */
+
 console.log("Hello GitHub");
